f4556f89f2fe57d70ac3e233f1b66559348e5183
Alejandro Benjumea Aguirre <alejandro_benjumea@pana.com.co>
2024-09-20 15:52:59 -0500

    Ajuste en modal

--- ticket-frontend/src/components/Modal.tsx
+++ ticket-frontend/src/components/Modal.tsx
@@ -3,7 +3,7 @@ export const Modal = () => {
   return (
     <>
       <div id="crud-modal" 
-           tabindex="-1" 
+           tabIndex={-1} 
            aria-hidden="true" 
            className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center 
            items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
